ecd214a3fa7bbdb7c17bbcd52b391c231dfb56d9
chenxia177217 <qingchen177217@163.com>
2022-08-08 22:46:24 +0800

    初始化完成

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,17 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+    transpileDependencies: true
 })
+module.export = {
+    configureWebpack: { //配置起别名
+        resolve: {
+            alias: {
+                'assets': './src/assets',
+                'common': './src/common',
+                'components': './src/components',
+                'network': './src/component/network',
+                'views': './src/views'
+            }
+        }
+    }
+}
